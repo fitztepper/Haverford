32e0679e66748bdf80a03631b5d8c026e9a79152
Fitz Tepper <fitztepper@me.com>
2012-01-04 18:13:10 -0500

    tests

---
+++ HaverfordTests/HaverfordTests.h
@@ -0,0 +1,13 @@
+//
+//  HaverfordTests.h
+//  HaverfordTests
+//
+//  Created by Fitz Tepper on 1/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface HaverfordTests : SenTestCase
+
+@end
