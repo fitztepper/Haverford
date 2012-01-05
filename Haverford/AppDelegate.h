5941117b6e1b2cf53c77aba2d6c54b2f32cb79e7
Fitz Tepper <fitztepper@me.com>
2012-01-05 01:03:56 -0500

    Added TestFlight Api

--- Haverford/AppDelegate.h
+++ Haverford/AppDelegate.h
@@ -17,5 +17,12 @@
 @property (strong, nonatomic) UITabBarController *tabBarController;
 
 @end
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
 
-testchange
+
+sa_handler
+=======
+>>>>>>> parent of 3d7b54c... testchange
+>>>>>>> parent of ece2d60... Revert 3d7b54c6e6a28c1aa39a087b8318b74215406460^..HEAD
